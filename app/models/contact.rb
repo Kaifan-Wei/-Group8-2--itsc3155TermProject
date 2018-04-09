8c169137bb6a48803650c82b1f44b7aab97a0874
Kaifan <kwei@uncc.edu>
2018-04-09 18:07:06 +0000

    fixed edit, update new and edit with _form

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,4 @@
 class Contact < ApplicationRecord
-    belongs_to :user
     validates :name, presence: true
     validates :number, format: { with: /\d{10}/,
         message: "Only allows numbers" }
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-    has_many :contacts
     validates :name, presence: true
     validates :password, presence: true, confirmation: true
     validates :email, presence: true
