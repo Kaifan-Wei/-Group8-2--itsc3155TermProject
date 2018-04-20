9621865459d3e121792ef97b943887a986171bfd
Kaifan <kwei@uncc.edu>
2018-04-20 19:30:59 +0000

    add a users model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180402183725) do
+ActiveRecord::Schema.define(version: 20180420193003) do
 
   create_table "contacts", force: :cascade do |t|
     t.string "name"
     t.string "number"
     t.string "address"
     t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.string "name"
+    t.string "oauth_token"
+    t.datetime "oauth_expires_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
