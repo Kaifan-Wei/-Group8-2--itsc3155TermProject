ab2f866204770e8060493c9d6ca5560b2de35778
Kaifan <kwei@uncc.edu>
2018-04-20 18:59:57 +0000

    destroy user model and controller

--- app/models/user.rb
+++
@@ -1,5 +0,0 @@
-class User < ApplicationRecord
-    validates :name, presence: true
-    validates :password, presence: true, confirmation: true
-    validates :email, presence: true
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180407052956) do
+ActiveRecord::Schema.define(version: 20180402183725) do
 
   create_table "contacts", force: :cascade do |t|
     t.string "name"
     t.string "number"
     t.string "address"
     t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
