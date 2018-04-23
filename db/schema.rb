11015db69ca450c8150f89629d15b5452e9d4a9c
Jason <jason.wei.kf@gmail.com>
2018-04-23 15:16:17 -0400

    Merge branch 'Kaifan' of https://github.com/Kaifan-Wei/-Group8-2--itsc3155TermProject into Kaifan
    empty commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180407052956) do
+ActiveRecord::Schema.define(version: 20180420193003) do
 
   create_table "contacts", force: :cascade do |t|
     t.string "name"
@@ -22,9 +22,11 @@ ActiveRecord::Schema.define(version: 20180407052956) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
     t.string "name"
-    t.string "email"
-    t.string "password"
+    t.string "oauth_token"
+    t.datetime "oauth_expires_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
